86f516b381ffb6085a1bcf2a644a1d1b6eef0386
Alex Eidt <alex.eidt@outlook.com>
2021-05-21 10:46:08 -0700

    Version 1.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', mode='r') as fh:
     long_description = fh.read()
 
 setuptools.setup(name='uwtools',
-      version='1.5.3',
+      version='1.6.0',
       description='Easy data parsing for courses at the University of Washington',
       long_description=long_description,
       long_description_content_type='text/markdown',
