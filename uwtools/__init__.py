3cc29098284ff0f11daadd6829a99c8d9a658a67
Alex Eidt <alex.eidt@outlook.com>
2021-05-21 12:39:56 -0700

    Updated __init__

--- uwtools/__init__.py
+++ uwtools/__init__.py
@@ -3,8 +3,6 @@
 from .parse_courses import parse_catalogs as course_catalogs
 from .parse_courses import get_departments as departments
 
-from .parse_schedules import quarter_dates as get_quarter_ranges
-from .parse_schedules import get_quarter
 from .parse_schedules import gather as time_schedules
 from .parse_schedules import get_academic_year as academic_year
 
